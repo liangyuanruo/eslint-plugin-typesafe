1c57734bae3fa572d2076bb999f91aebc92ae1b1
Yuanruo Liang <yuanruo@open.gov.sg>
2021-01-18 16:10:38 +0800

    feat(promise-catch): take care of ArrowFunctionExpressions

--- tests/rules/promise-catch.js
+++ tests/rules/promise-catch.js
@@ -54,6 +54,46 @@ eslintTester.run("promise-catch", rule, {
         
         y().catch(e => console.error(e)).then(x => x).catch(e => console.error(e))`,
         parserOptions: { ecmaVersion: 8, sourceType: "module" }
+    },
+    {
+        // Promise must end with catch
+        code: `
+        const y = async () => {
+            try {
+                await z()
+            } catch (e) {
+                console.error(e)
+            }
+        }
+
+        y().catch(e => console.error(e))`,
+        parserOptions: { ecmaVersion: 8, sourceType: "module" }
+    }, {
+        // Promise chains must end with catch
+        code: `
+        const y = async () => {
+            try {
+                await z()
+            } catch (e) {
+                console.error(e)
+            }
+        }
+
+        y().then(x => x).catch(e => console.error(e))`,
+        parserOptions: { ecmaVersion: 8, sourceType: "module" }
+    }, {
+        // Allow interleaving catch
+        code: `
+        const y = async () => {
+            try {
+                await z()
+            } catch (e) {
+                console.error(e)
+            }
+        }
+
+        y().catch(e => console.error(e)).then(x => x).catch(e => console.error(e))`,
+        parserOptions: { ecmaVersion: 8, sourceType: "module" }
     }
   ],
   invalid: [
@@ -64,7 +104,7 @@ eslintTester.run("promise-catch", rule, {
             const x = await z()
             return x
         }
-        
+
         y()`,
         parserOptions: { ecmaVersion: 8, sourceType: "module" },
       errors: [
@@ -73,16 +113,42 @@ eslintTester.run("promise-catch", rule, {
     }, {
         // Dangling "then" expression at the end
         code: `
-          async function abc () {
+          const y = async () => {
               const x = await z()
               return x
           }
-          
-          abc().then(x => x)`,
+
+          y().then(x => x)`,
           parserOptions: { ecmaVersion: 8, sourceType: "module" },
         errors: [
           { message: "Missing catch statement." }
         ],
-      }
+      }, {
+        // Missing catch
+        code: `
+          const y = async () => {
+              const x = await z()
+              return x
+          }
+
+          y()`,
+          parserOptions: { ecmaVersion: 8, sourceType: "module" },
+        errors: [
+          { message: "Missing catch statement." }
+        ],
+      }, {
+          // Dangling "then" expression at the end
+          code: `
+            const y = async () => {
+                const x = await z()
+                return x
+            }
+
+            y().then(x => x)`,
+            parserOptions: { ecmaVersion: 8, sourceType: "module" },
+          errors: [
+            { message: "Missing catch statement." }
+          ],
+        }
   ]
 });
