d0022f85349ef4e36216a417648f9bd89c54ebc4
Yuan Ruo <yuanruo@data.gov.sg>
2021-01-19 22:29:17 +0800

    feat/promise-catch: add cases where a synchronous function returns a new Promise (#6)
    Co-authored-by: Yuanruo Liang <yuanruo@open.gov.sg>

--- tests/rules/promise-catch.js
+++ tests/rules/promise-catch.js
@@ -94,7 +94,16 @@ eslintTester.run("promise-catch", rule, {
 
         y().catch(e => console.error(e)).then(x => x).catch(e => console.error(e))`,
         parserOptions: { ecmaVersion: 8, sourceType: "module" }
-    }
+    }, {
+      // Synchronous function that returns Promise
+      code: `
+      function f() {
+        return new Promise((resolve, reject) => resolve(1))
+      }
+
+      f().catch(e => console.error(e))`,
+      parserOptions: { ecmaVersion: 8, sourceType: "module" }
+    },
   ],
   invalid: [
     {
@@ -162,7 +171,37 @@ eslintTester.run("promise-catch", rule, {
         errors: [
           { message: "Missing catch statement." }
         ],
-    }
+      }, {
+        // Alternating catch-then returning a new Promise
+        code: `
+          function f() { return new Promise((resolve, reject) => resolve(1)) }
+
+          f().then(x => x).catch(console.error).then(x => x)`,
+          parserOptions: { ecmaVersion: 8, sourceType: "module" },
+        errors: [
+          { message: "Missing catch statement." }
+        ],
+      }, {
+        // Alternating catch-then returning a Promise literal
+        code: `
+          function f() { return Promise.reject(123) }
+
+          f().then(x => x).catch(console.error).then(x => x)`,
+          parserOptions: { ecmaVersion: 8, sourceType: "module" },
+        errors: [
+          { message: "Missing catch statement." }
+        ],
+      }, {
+        // Alternating catch-then returning a Promise literal
+        code: `
+          function f() { return Promise.resolve(123) }
+
+          f().then(x => x).catch(console.error).then(x => x)`,
+          parserOptions: { ecmaVersion: 8, sourceType: "module" },
+        errors: [
+          { message: "Missing catch statement." }
+        ],
+      }
       // TODO: Instance method calls, inheritance
       // {
       //   code: `
